a755fb8529f6779cacff2d45feab01ee1755cdb7
Jakob Langseth <jakob.langseth@gmail.com>
2024-03-07 23:12:09 +0100

    Retrying deployment push

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['superboss@admin.com', 'jakob.langseth@gmail.com']
-    LANGUAGES = ['en', 'es', 'nb']
+    ADMINS = ['jakob.langseth@gmail.com']
+    LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 25
