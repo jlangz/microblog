6cf1e1750cd7cd283b0d6fb067686a54b477f4d4
Jakob Langseth <jakob.langseth@gmail.com>
2024-03-07 18:19:39 +0100

    heroku deployment changes

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
@@ -21,4 +19,5 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    POSTS_PER_PAGE = 5
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    POSTS_PER_PAGE = 25
