cb9cc3eceb5144293f9fb2742c4f04cbb2e91599
Jakob Langseth <jakob.langseth@gmail.com>
2024-03-08 00:58:02 +0100

    Downgrading elastic transport

--- app/search.py
+++ app/search.py
@@ -21,7 +21,7 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
-        query={'multi_match': {'query': query, 'fields': ['*']}},
+        body={'query': {'multi_match': {'query': query, 'fields': ['*']}}},
         from_=(page - 1) * per_page,
         size=per_page)
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
